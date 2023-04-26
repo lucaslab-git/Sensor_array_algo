547c6565853d58b96bb3ed3a8791fa1482c3931d
lucaslab-git <schweda.luca@gmx.net>
2023-04-26 09:39:53 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,68 @@
+#include <Arduino.h>
+
+int sensorvalues[] = {20, 20, 20, 21, 20, 30, 30, 31, 30, 21, 20, 20, 20, 20};
+
+
+struct Farbe
+{
+  int untere_grenze;
+  int obere_grenze;
+  int referenzwert;
+};
+
+struct Farbe schwarz, weiss, gruen;
+
+void berechnung_werte(int neuer_wert, Farbe color) {
+  color.referenzwert = (2 * color.referenzwert + neuer_wert) / 3;
+
+  color.untere_grenze = color.referenzwert - 2;
+  color.obere_grenze = color.referenzwert + 2;
+}
+
+void validierung_werte(Farbe color, String scolor) {
+    Serial.println(scolor + ":");
+    for (size_t i = 0; i < 14; i++)
+    {
+     
+      if (sensorvalues[i] <= color.obere_grenze && sensorvalues[i] >= color.untere_grenze)
+      {
+        berechnung_werte(sensorvalues[i], color);
+       
+        Serial.print(sensorvalues[i]);
+        Serial.print(", ");
+      }
+    }
+    Serial.println("");
+    
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Bekommener Array:");
+  for (size_t i = 0; i < 14; i++)
+  {
+    Serial.print("|" + String(sensorvalues[i]));
+  }
+  Serial.println("");
+  
+
+  schwarz.referenzwert = 30;
+  schwarz.untere_grenze = 28;
+  schwarz.obere_grenze = 32;
+
+  weiss.referenzwert = 20;
+  weiss.untere_grenze = 18;
+  weiss.obere_grenze = 22;  
+
+  gruen.referenzwert = 26;
+   validierung_werte(weiss, "Weiss");
+   validierung_werte(schwarz, "schwarz");
+ 
+
+}
+
+void loop() {
+ 
+  
+}
